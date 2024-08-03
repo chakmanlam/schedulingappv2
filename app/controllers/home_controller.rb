332cf3fdea2784e03bb5ab2971d5a425429f0a87
chakmanlam <chakman@upgrowth.one>
2024-08-03 12:50:17 -0400

    login home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
-  # before_action :authenticate_user!
 
   def show
+  end
+
+  def dashboard
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   end
   devise_for :users
 
+  authenticated :user do
+    root "home#dashboard", as: :authenticated_root
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
